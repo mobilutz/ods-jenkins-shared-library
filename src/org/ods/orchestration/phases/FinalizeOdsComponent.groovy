a66ce6ffe04bc78368c841b71418c06e8d621d54
clemensutschig <clemens.utschig-utschig@boehringer-ingelheim.com>
2022-01-26 13:02:12 +0100

    rm export back

--- src/org/ods/orchestration/phases/FinalizeOdsComponent.groovy
+++ src/org/ods/orchestration/phases/FinalizeOdsComponent.groovy
@@ -37,7 +37,7 @@ class FinalizeOdsComponent {
         def isRMrepo = (RELEASE_MANAGER_REPO_ID == repo.id)
 
         if (isRMrepo) {
-            componentSelector = null
+            componentSelector = ''
         } else {
             verifyDeploymentsBuiltByODS(repo, componentSelector)
         }
@@ -68,15 +68,13 @@ class FinalizeOdsComponent {
                         )
                         filesToStage << OpenShiftService.TAILOR_FILE_NAME
                     }
-                    if (!isRMrepo) {
-                        os.tailorExport(
-                            !isRMrepo ? project.targetProject : "${project.key}-cd",
-                            componentSelector,
-                            envParams,
-                            OpenShiftService.EXPORTED_TEMPLATE_FILE
-                        )
-                        filesToStage << OpenShiftService.EXPORTED_TEMPLATE_FILE
-                    }
+                    os.tailorExport(
+                        !isRMrepo ? project.targetProject : "${project.key}-cd",
+                        componentSelector,
+                        envParams,
+                        OpenShiftService.EXPORTED_TEMPLATE_FILE
+                    )
+                    filesToStage << OpenShiftService.EXPORTED_TEMPLATE_FILE
                     logger.debugClocked("export-ocp-${repo.id}", (null as String))
                 } else {
                     commitMessage = "ODS: Export Openshift deployment state " +
